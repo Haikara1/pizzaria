5d017a8464de57b911adf066b0eb846723605186
Daniel Lima <danisfilholima@gmail.com>
2026-09-01 01:04:48 -0300

    homepage pizzaria

---
+++ src/components/MenuShowcase/menuData.js
@@ -0,0 +1,4 @@
+export const pizzas=[
+ {name:'A Essencial',description:'Molho de tomates, mozzarella fresca, manjericão e azeite extravirgem.',price:'R$ ---',position:'22% center'},
+ {name:'Bosco',description:'Creme de queijo, cogumelos tostados, ervas frescas e finalização da casa.',price:'R$ ---',position:'50% center'},
+ {name:'Horta',description:'Vegetais assados, queijo fresco, tomate confit e folhas aromáticas.',price:'R$ ---',position:'82% center'}]
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import Header from '../../components/Header/Header'
+import Hero from '../../components/Hero/Hero'
+import About from '../../components/About/About'
+import Experience from '../../components/Experience/Experience'
+import MenuShowcase from '../../components/MenuShowcase/MenuShowcase'
+import Gallery from '../../components/Gallery/Gallery'
+import InstagramSection from '../../components/InstagramSection/InstagramSection'
+import FinalCTA from '../../components/FinalCTA/FinalCTA'
+import Footer from '../../components/Footer/Footer'
+export default function Home(){return <><Header/><main><Hero/><About/><Experience/><MenuShowcase/><Gallery/><InstagramSection/><FinalCTA/></main><Footer/></>}
